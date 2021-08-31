c5b53144d194dd585311d633ffe90ae93580f559
Endo Takkaaki <endot1231@gmail.com>
2021-08-31 22:54:23 +0900

    Add header of blade.

---
+++ resources/views/component/header.blade.php
@@ -0,0 +1,30 @@
+
+<header class="bg-base header_height">
+  <div class="container">
+    <div class="row pt-3 text-white">
+      <div class="col-4 title">
+        <i class="fas fa-book mr-1 fa-lg"></i>読書アウトプット
+      </div>
+      <div class="col-8 text-right login">
+        <a href="/login" class="text-white">ログイン / 新規登録</a>
+      </div>
+    </div>
+  </div>
+</header> 
+
+
+@push('css')
+<style type="css">
+.header_height{
+  height: 48px;
+}
+
+.title{
+  font-size:14px;
+}
+
+.login{
+  font-size:14px;
+}
+</style>
+@endpush
