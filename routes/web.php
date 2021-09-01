dd007a046584a8ce472edf2b21eb068434b7f4b0
Endo Takkaaki <endot1231@gmail.com>
2021-09-01 23:43:59 +0900

    Fix web route file.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OutputController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any?}',function(){
-    return view('index');
-})->where('any', '.+');
+Route::get('/', [HomeController::class,'index']);
+Route::resource('output',OutputController::class);
+Auth::routes();
