40757c07a44a169f5b2ebf63fad32419ece8d417
Endo Takkaaki <endot1231@gmail.com>
2021-09-07 00:20:11 +0900

    add image style for image of output card

--- resources/views/organisms/list_output_card.blade.php
+++ resources/views/organisms/list_output_card.blade.php
@@ -10,12 +10,21 @@
 </div>
 
 @push('css')
-<style>
+<style type="text/css">
     .output_card {
         cursor : pointer;
         transition:box-shadow 0.3s, transform 0.3s;
     }
     
+    .output_card_image {
+        width: 70px;
+        height: 70px;
+        border-radius: 50%;
+        background-position: center center;
+        cursor: pointer;
+        transition-duration: 0.3s;
+    }
+
     .output_card_hover_animation:hover {
         box-shadow:0 6px 14px rgba(0, 0, 0, 0.24);
         transform:translate(0, -2px);
