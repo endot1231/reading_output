32d4520255866d8a19d1c7c9861e65c0ceeab125
Endo Takkaaki <endot1231@gmail.com>
2021-09-18 23:05:42 +0900

    Add factory of user.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,8 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('123456789'), // password
+            'password' => Hash::make('12345678'), // password
+            'profile_id' =>1, 
             'remember_token' => Str::random(10),
         ];
     }
