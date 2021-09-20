e8423bd5f0bb9bd0e3f5d934628821278734b203
Endo Takkaaki <endot1231@gmail.com>
2021-09-20 15:37:33 +0900

    Fix index blade.

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.base')
 
 @section('content')
-@include('organisms.list_output_card',['outputs' => $outputs])
+@include('organisms.search_area',['url'=>route('index')])
+@include('organisms.list_output_card',['outputs' => $outputs,'title'=>"アウトプット一覧"])
 @endsection
 
