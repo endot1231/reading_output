b80bdb617c05ac5a2ca1ff3c28f5b037c2937015
Endo Takkaaki <endot1231@gmail.com>
2021-09-21 23:52:49 +0900

    Add fucntion of keyword inputed textarea.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use App\Models\OutPut;
+use App\Models\History;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -14,12 +17,34 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-        $outputs =OutPut::paginate(5);
-        return view('page.index',['outputs'=>$outputs]);
+        if($request->has('keyword')){
+            $keyword = $request->get('keyword');
+            $request->session()->put('keyword',$keyword);
+            $outputs =OutPut::with('user')->where('title','like', "%$keyword%")->orderBy('created_at','desc')->paginate(10);
+        }else{
+            $request->session()->forget('keyword');
+            $outputs =OutPut::with('user')->orderBy('created_at','desc')->paginate(10);
+        }
+        return view('page.index',['outputs'=>$outputs]);   
     }
 
-    public function show(){
+    public function history(Request $request){
+        if (!Auth::check()) {
+            abort('404');
+        }
 
+        if($request->has('keyword')){
+            $keyword = $request->get('keyword');
+            $request->session()->put('keyword',$keyword);
+
+            $histories =  History::where('user_id',Auth::id())->whereHas('output',function($query)use($keyword){
+                $query->where('title','like', "%$keyword%");
+            })->orderBy('created_at','desc')->paginate(10);
+        }else{
+            $request->session()->forget('keyword');
+            $histories =  History::where('user_id',Auth::id())->has('output')->orderBy('created_at','desc')->paginate(10);
+        }
+        return view('page.history',['histories'=>$histories]);   
     }
 
 }
