963c4a8d13ff2d5009a65e8ab750bc40c6fba904
Endo Takkaaki <endot1231@gmail.com>
2021-09-24 00:19:56 +0900

    fix path of project folder.

--- scripts_for_deploy/preparation.sh
+++ scripts_for_deploy/preparation.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-find /var/www/app -type f -exec chmod 664 {} \;
-find /var/www/app -type d -exec chmod 774 {} \;
-chown -R ec2-user:nginx /var/www/app
+mkdir -p /usr/share/nginx/html/reading_output/
+find /usr/share/nginx/html/reading_output/ -type f -exec chmod 664 {} \;
+find /usr/share/nginx/html/reading_output/ -type d -exec chmod 774 {} \;
+chown -R ec2-user:nginx /usr/share/nginx/html/reading_output/
 
-chmod -R 775 /var/www/app/storage
-chmod -R 775 /var/www/app/bootstrap/cache
+chmod -R 775 /usr/share/nginx/html/reading_output/storage
+chmod -R 775 /usr/share/nginx/html/reading_output/cache
 
 # db migrate
-cd /var/www/app
+cd /usr/share/nginx/html/reading_output/
 php artisan migrate
-
-# composer
-# cd /var/www/app
-# composer install
+composer install
